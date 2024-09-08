31b87b9cf9bd513930a9cb7939b6d2d3fba5f0cd
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2024-09-09 00:12:36 +0900

    中身を使うもの

---
+++ Novice/mouse.cpp
@@ -0,0 +1,27 @@
+#include "mouse.h"
+ #include <Novice.h>
+void mouse::GetMouse(int mouseX, int mouseY) {
+
+
+	Novice::GetMousePosition(&mouseX, &mouseY);
+
+	mouseY *= -1;
+	mouseY += 720;
+
+	
+
+	 
+}
+
+bool mouse::leftGetMouse() {
+
+	bool num = Novice::IsTriggerMouse(0);
+
+	return num;
+}
+
+bool mouse::RightGetMouse() {
+	bool num = Novice::IsTriggerMouse(1);
+
+	return num;
+}
---
+++ Novice/mouse.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class mouse {
+private:
+	int mouseX, mouseY;
+
+public:
+	
+	void GetMouse(int mouseX, int mouseY);
+	bool leftGetMouse();
+	bool RightGetMouse();
+
+
+};
