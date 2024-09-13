92c36faf9e1b258388b08f11d540a8fc614f24ea
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2024-09-13 17:12:34 +0900

    stageSelect

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -3,7 +3,7 @@
 #include<Player.h>
 #include"Goal.h"
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "3043_リードボール";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -41,14 +41,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	} Box;
 
 	Box Item[4] = {
-	    {100, 100, 100, 100},
-        {300, 100, 100, 100},
-        {100, 300, 100, 100},
-        {300, 300, 100, 100}
+	    {100, 300, 100, 100},
+        {300, 300, 100, 100},
+        {500, 300, 100, 100},
+        {700, 300, 100, 100}
     };
 
 	int titlesprite = Novice::LoadTexture("./Resorce/Title.png");
 	int gamedescriptionsprite = Novice::LoadTexture("./Resorce/Game_Description.png");
+
+	int bg = Novice::LoadTexture("./Resorce/Back.png");
+			  
+	int select1 = Novice::LoadTexture("./Resorce/select1.png");
+	int select2 = Novice::LoadTexture("./Resorce/select2.png");
+	int select3 = Novice::LoadTexture("./Resorce/select3.png");
+	int selectHelp = Novice::LoadTexture("./Resorce/help.png");
 
 	std::unique_ptr<mouse> Mouse;
 	Mouse = std::make_unique<mouse>();
@@ -85,12 +92,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				//ゲーム説明// ステージ１
 			    // ステージ2// ステージ3の順番
 			    for (int i = 0; i < 4; i++) {
-				    Novice::DrawBox((int)Item[i].Position.x, (int)Item[i].Position.y, (int)Item[i].Size.x, (int)Item[i].Size.y, 0.0f, BLUE, kFillModeSolid);
-			    
+				    
 					if (Mouse->leftGetMouse() && Mouse->IsMouseOverBox((int)Item[i].Position.x, (int)Item[i].Position.y, (int)Item[i].Size.x, (int)Item[i].Size.y)) {
 					    Scene = i + 3;
 				    }
 				} 
+
+				 Novice::DrawSprite(0, 0, bg, 1.0f, 1.0f, 0.0f, WHITE);
+				 Novice::DrawSprite((int)Item[0].Position.x, (int)Item[0].Position.y, selectHelp, 1.0f, 1.0f, 0.0f, WHITE);
+			     Novice::DrawSprite((int)Item[1].Position.x, (int)Item[1].Position.y, select1, 1.0f, 1.0f, 0.0f, WHITE);
+				 Novice::DrawSprite((int)Item[2].Position.x, (int)Item[2].Position.y, select2, 1.0f, 1.0f, 0.0f, WHITE);
+			     Novice::DrawSprite((int)Item[3].Position.x, (int)Item[3].Position.y, select3, 1.0f, 1.0f, 0.0f, WHITE);
+			    
+
 			    break;
 		    case 2:
 			    // クリア
